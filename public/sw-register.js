9f022bd26e576035224e2286a7ae4f23caae74ee
Mincorey <antonov.oleg.1987@gmail.com>
2026-05-24 05:33:26 +0000

    fix: move SW registration to external file to fix Vite parse5 build error
    - Extract inline SW cleanup/registration script from index.html
      into public/sw-register.js to avoid Vite HTML parser (parse5) bug
      with eof-in-element-that-can-contain-only-text error
    - index.html now references /sw-register.js as an external script
    - Vite copies public/ files as-is, so sw-register.js bypasses the HTML parser

---
+++ public/sw-register.js
@@ -0,0 +1,28 @@
+(function () {
+  var CURRENT = "apsny-mfl-v4";
+  if (!("serviceWorker" in navigator)) return;
+
+  caches.keys().then(function (keys) {
+    var hasOld = keys.some(function (k) { return k !== CURRENT; });
+    if (hasOld) {
+      navigator.serviceWorker.getRegistrations()
+        .then(function (regs) {
+          return Promise.all(regs.map(function (r) { return r.unregister(); }));
+        })
+        .then(function () {
+          return Promise.all(keys.map(function (k) { return caches.delete(k); }));
+        })
+        .then(function () {
+          window.location.reload(true);
+        });
+    } else {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js");
+      });
+    }
+  }).catch(function () {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker.register("/sw.js");
+    });
+  });
+})();
